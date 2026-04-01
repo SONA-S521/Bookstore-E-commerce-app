464f553b5c3751387ba7d2b51e0616d4680e6857
SONA-S521 <sonasr06@gmail.com>
2026-04-01 14:25:38 +0530

    Create books.js

---
+++ models/books.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+const bookSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  author: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true, default: 10 }
+}, { timestamps: true });
+const Book = mongoose.model('Book', bookSchema);
+export default Book;
